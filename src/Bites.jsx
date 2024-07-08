955a130d171897a17a2fe5a9554f393d4796d472
Piotray <159956456+Piotray@users.noreply.github.com>
2024-07-08 23:03:31 +0200

    Fix Bites.jsx

--- src/Bites.jsx
+++ src/Bites.jsx
@@ -5,22 +5,22 @@ const Bites = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   const pizzaItems = [
-    { id: 1, image: '/assets/bites/pizza1.jpg', header: 'Pepperoni Pizza', text: 'Pepperoni, Salami, Mushrooms', price: '$10' },
+    { id: 1, image: './assets/bites/pizza1.jpg', header: 'Pepperoni Pizza', text: 'Pepperoni, Salami, Mushrooms', price: '$10' },
     { id: 2, image: './assets/bites/pizza2.jpg', header: 'Carbonara Pizza', text: 'Bacon, Spinach, Feta Cheese', price: '$12' },
-    { id: 3, image: '/assets/bites/pizza3.jpg', header: 'Capricciosa Pizza', text: 'Ham, Pineapple, Red Onions', price: '$12' },
+    { id: 3, image: './assets/bites/pizza3.jpg', header: 'Capricciosa Pizza', text: 'Ham, Pineapple, Red Onions', price: '$12' },
   
   ];
 
   const pastaItems = [
-    { id: 1, image: '/assets/bites/pasta1.jpg', header: 'Spaghetti Carbonara', text: 'Spaghetti, Pancetta, Egg', price: '$8' },
-    { id: 2, image: '/assets/bites/pasta2.jpg', header: 'Fettuccine Alfredo', text: 'Fettuccine, Cream, Parmesan', price: '$9' },
-    { id: 3, image: '/assets/bites/pasta3.jpg', header: 'Penne Arrabbiata', text: 'Penne, Tomato Sauce, Garlic', price: '$9' },
+    { id: 1, image: './assets/bites/pasta1.jpg', header: 'Spaghetti Carbonara', text: 'Spaghetti, Pancetta, Egg', price: '$8' },
+    { id: 2, image: './assets/bites/pasta2.jpg', header: 'Fettuccine Alfredo', text: 'Fettuccine, Cream, Parmesan', price: '$9' },
+    { id: 3, image: './assets/bites/pasta3.jpg', header: 'Penne Arrabbiata', text: 'Penne, Tomato Sauce, Garlic', price: '$9' },
   ];
 
   const saladItems = [
-    { id: 1, image: '/assets/bites/salad1.jpg', header: 'Panzanella Salad', text: 'Mixed Greens, Tomatoes, Olives', price: '$7' },
-    { id: 2, image: '/assets/bites/salad2.jpg', header: 'Insalata di Riso', text: 'Rice, Tuna, Red Peppers', price: '$8' },
-    { id: 3, image: '/assets/bites/salad3.jpg', header: 'Insalata di Farro', text: 'Farro, Arugula, Roasted Vegetables', price: '$7' },
+    { id: 1, image: './assets/bites/salad1.jpg', header: 'Panzanella Salad', text: 'Mixed Greens, Tomatoes, Olives', price: '$7' },
+    { id: 2, image: './assets/bites/salad2.jpg', header: 'Insalata di Riso', text: 'Rice, Tuna, Red Peppers', price: '$8' },
+    { id: 3, image: './assets/bites/salad3.jpg', header: 'Insalata di Farro', text: 'Farro, Arugula, Roasted Vegetables', price: '$7' },
   ];
 
   const renderItems = (items) => {
